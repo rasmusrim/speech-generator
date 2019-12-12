94b6089870115e76cd8a6b3949892b65285567d8
Rasmus Rimestad <rasmusrim@gmail.com>
2019-12-12 20:54:37 +0100

    Added build folder

---
+++ build/precache-manifest.9a2dc5fb8f540c0b33bc83e58eb03744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39bdeef91fa55e2df8e473283451f91",
+    "url": "/speech-generator/index.html"
+  },
+  {
+    "revision": "c6ee1e8e3186b1866663",
+    "url": "/speech-generator/static/css/main.0ce4883e.chunk.css"
+  },
+  {
+    "revision": "7fa44837ac108c8ee82e",
+    "url": "/speech-generator/static/js/2.8a2a9c7f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/speech-generator/static/js/2.8a2a9c7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6ee1e8e3186b1866663",
+    "url": "/speech-generator/static/js/main.017d3fc0.chunk.js"
+  },
+  {
+    "revision": "775f57aed3a75d2f3b14",
+    "url": "/speech-generator/static/js/runtime-main.d3144ade.js"
+  }
+]);
