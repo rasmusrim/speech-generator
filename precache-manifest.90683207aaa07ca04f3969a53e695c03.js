88f8b1e8d437c85b7a26ed45e579d7a8fee3f4bb
Rasmus Rimestad <rasmusrim@gmail.com>
2019-12-12 20:24:26 +0100

    Updates

---
+++ precache-manifest.90683207aaa07ca04f3969a53e695c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b76f8749ea7aeab95586ea5dd8e070",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b61c28b054e89ae4a76f",
+    "url": "/static/css/main.0ce4883e.chunk.css"
+  },
+  {
+    "revision": "7fa44837ac108c8ee82e",
+    "url": "/static/js/2.8a2a9c7f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8a2a9c7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b61c28b054e89ae4a76f",
+    "url": "/static/js/main.53bd1240.chunk.js"
+  },
+  {
+    "revision": "0afcce2384baa1793bd5",
+    "url": "/static/js/runtime-main.b8546d7c.js"
+  }
+]);
